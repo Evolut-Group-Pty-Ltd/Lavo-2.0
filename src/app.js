2b882a586754852001dfc40d681955671270fb82
Alex Chernov <alexusblack@gmail.com>
2021-07-07 08:19:09 +1000

    speed multiplier

--- src/app.js
+++ src/app.js
@@ -154,7 +154,7 @@ const onLoadingComlete = () => {
         return
       }
       const delta = (end.y - start.y) / window.innerHeight * 50
-      updateScrollPosition(scrollPosition - delta)
+      updateScrollPosition(scrollPosition - delta * 0.2)
       gl.updatePointer(end)
     })
 
